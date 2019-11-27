ee11d21c1109aefbefb3f3cffa100f1dd20798b4
strumswell <philipp.bukkit@gmail.com>
2019-11-27 14:29:01 +0100

    added email confirmation on user creation

---
+++ util/confirmMail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  }
+});
+
+function sendEmailConfirmation(receiver, id) {
+  var mailOptions = {
+    from: 'Webshop XYZ <infodsp.bot@gmail.com>',
+    to: receiver,
+    subject: 'Bestätige deine E-Mail!',
+    html: '<h3>Fast fertig!</h3> Bestätige deine E-Mail mit einem Klick.<br><a href="https://api.bolte.cloud/v1/users/'+id+'/confirmEmail">Jetzt bestätigen.</a>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + receiver + "-" + id + " :::: " + info.response);
+    }
+  });
+}
+
+module.exports.sendEmailConfirmation = sendEmailConfirmation;
